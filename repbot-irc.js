e1508a6526ee60af332062784aec493cd3dadb7e
repkam09 <mark@repkam09.com>
2015-10-06 22:51:44 -0400

    multichannel start, updated wordlist

--- repbot-irc.js
+++ repbot-irc.js
@@ -9,32 +9,37 @@ console.log(secrets);
 var config = {
     server: "irc.twitch.tv",
     botName: "repkbot",
-    channel: "#lowride_mcclyde"
+    channel: ["#repkam09", "#lowride_mcclyde"]
 };
 
 console.log("Starting up repbot on irc...");
 
 // Create the bot name
 var bot = new irc.Client(config.server, config.botName, {
-    channels: [config.channel],
+    channels: config.channel,
     debug: true,
     password: secrets.oauth,
     username: config.botName
 });
 
-bot.addListener('message', function (from, to, message) {
+
+config.channel.forEach(function(channelName) {
+bot.addListener('message'+ channelName , function (from, to, messageObj) {
+    console.log(channelName + " " + from + " => " + to + " " + JSON.stringify(messageObj));
+    var message = messageObj.args[2];
+    console.log(channelName + ":" + message);
     if (message.indexOf("told") > -1) {
         var told = randomInt(0, wordlists.toldlist.length - 1);
-        bot.say(config.channel, wordlists.toldlist[told]);
+        bot.say(channelName, wordlists.toldlist[told]);
     }
 
     if (message.indexOf("rekt") > -1) {
         var rekt = randomInt(0, wordlists.rektlist.length - 1);
-        bot.say(config.channel, wordlists.rektlist[rekt]);
+        bot.say(channelName, wordlists.rektlist[rekt]);
     }
 
     if (message.indexOf("!hype") === 0) {
-        bot.say(config.channel, "/me HYPE THRUSTERS ACTIVATED!! (http://gfycat.com/ActualFeistyBettong)");
+        bot.say(channelName, "/me HYPE THRUSTERS ACTIVATED!! (http://gfycat.com/ActualFeistyBettong)");
     }
 
     if (message.indexOf("!reloadconfig") === 0) {
@@ -42,18 +47,19 @@ bot.addListener('message', function (from, to, message) {
         if (from === "repkam09") {
             try {
                 wordlists = JSON.parse(fs.readFileSync('wordlists.json', 'utf8'));
-                bot.say(config.channel, "/me configuration reloaded successfully");
+                bot.say(channelName, "/me configuration reloaded successfully");
             } catch (error) {
-                bot.say(config.channel, "/me error during configuration reloaded");
+                bot.say(channelName, "/me error during configuration reloaded");
             }
         } else {
-            bot.say(config.channel, "sorry, only 'repkam09' can use this method.");
+            bot.say(channelName, "sorry, only 'repkam09' can use this method.");
         }
     }
 
     if (message === "!help") {
-        bot.say(config.channel, "Commands: !hype, !rekt, !told, !help");
+        bot.say(channelName, "Commands: !hype, !rekt, !told, !help");
     }
+})
 });
 
 console.log("> Add Chat Listener for Errors");
